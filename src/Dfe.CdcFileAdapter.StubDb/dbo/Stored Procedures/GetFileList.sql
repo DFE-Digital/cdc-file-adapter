4c9a5669ddb0ba30b2f692e0a1a0c56377d4eed9
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-01-04 13:57:37 +0000

    Stub database and azure template update.

---
+++ src/Dfe.CdcFileAdapter.StubDb/dbo/Stored Procedures/GetFileList.sql
@@ -0,0 +1,26 @@
+﻿-- =============================================
+-- Author:		Matt Middleton
+-- Create date: 2021-01-04
+-- =============================================
+CREATE PROCEDURE dbo.GetFileList 
+	@EstablishmentID INT, 
+	@TypeID TINYINT
+AS
+BEGIN
+    
+	SELECT
+		CAST(NULL AS INT) AS CDCFeild,
+		f.EstablishmentId AS EstablishmentID,
+		CAST(NULL AS VARCHAR(50)) AS SupplierKeyID,
+		CAST(NULL AS VARCHAR(100)) AS EstablishmentName,
+		CAST(NULL AS VARCHAR(50)) AS FileTypeDescription,
+		f.SiteVisitDate,
+		f.FileName,
+		f.FileURL
+	FROM Files as f
+	WHERE
+		f.EstablishmentId = @EstablishmentID
+			AND
+		f.FileType = @TypeID;
+
+END
---
+++ src/Dfe.CdcFileAdapter.StubDb/dbo/Tables/Files.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE dbo.Files (
+    Id              INT           IDENTITY (1, 1) NOT NULL,
+    EstablishmentId INT           NULL,
+    SiteVisitDate   DATE          NULL,
+    [FileName]        VARCHAR (50)  NOT NULL,
+    FileURL         VARCHAR (MAX) NOT NULL,
+    FileType        TINYINT       NULL,
+    CONSTRAINT PK_Files PRIMARY KEY CLUSTERED (Id ASC)
+);
+
